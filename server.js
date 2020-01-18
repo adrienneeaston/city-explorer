f95e15594743d55b8b53c82091d3b3e5559a9486
Adrienne <adrienneeaston@gmail.com>
2020-01-18 00:36:06 -0800

    basic hello world server

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3001;
-
-// app.use(express.static('./public'));
+const PORT = process.env.PORT || 3000;
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
